687987ff9969d78c0866cc2ae05e65ae3811832f
Daniel Hölbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2017-03-14 14:38:05 +0100

    moved inputs list to bitmovin client

--- lib/bitmovin/encoding/inputs.rb
+++ lib/bitmovin/encoding/inputs.rb
@@ -1,8 +1,7 @@
 module Bitmovin::Encoding
   module Inputs
     def Inputs.list(limit = 100, offset = 0)
-      conn = Faraday.new(url: 'https://api.bitmovin.com')
-      response = conn.get '/v1/encoding/inputs', { limit: limit, offset: offset }
+      response = Bitmovin.client.get '/v1/encoding/inputs', { limit: limit, offset: offset }
       result = (JSON.parse(response.body))['data']['result']
       list = result['items'].map do |item|
         case item['type'].downcase
