7f77c4e6457485542954dfc10b3c5f9b0b822c90
Daniel Hölbling-Inzko <daniel.hoelbling-inzko@bitmovin.com>
2017-03-15 16:12:50 +0100

    Added codec configuration options for h264

--- lib/bitmovin/encoding/codec_configurations/h264_configuration.rb
+++ lib/bitmovin/encoding/codec_configurations/h264_configuration.rb
@@ -1,6 +1,9 @@
 module Bitmovin::Encoding::CodecConfigurations
   class H264Configuration < Bitmovin::ConfigurationResource
-    def initialize(hash)
-    end
+    init 'encoding/configurations/video/h264'
+
+    attr_accessor :id, :created_at, :modified_at, :name, :description
+    attr_accessor :width, :height, :bitrate, :rate, :profile, :bframes, :ref_frames, :qp_min, :qp_max, :mv_prediction_mode, :mv_search_range_max, :cabac, :max_bitrate, :min_bitrate, :bufsize
+    attr_accessor :min_gop, :max_gop, :level, :b_adaptive_strategy, :motion_estimation_method, :rc_look_ahead, :sub_me, :trellis, :partitions, :slices, :interlace_mode
   end
 end
