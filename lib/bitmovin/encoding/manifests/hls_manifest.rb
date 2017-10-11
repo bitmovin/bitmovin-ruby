719e1dbf381396dd7c4d3d7805e0c23009328abc
Daniel Hoelbling-Inzko <daniel@hoelbling-inzko.com>
2017-10-11 09:45:41 +0200

    Added outputs to hls manifests

--- lib/bitmovin/encoding/manifests/hls_manifest.rb
+++ lib/bitmovin/encoding/manifests/hls_manifest.rb
@@ -3,5 +3,18 @@ module Bitmovin::Encoding::Manifests
     init("/v1/encoding/manifests/hls")
 
     attr_accessor :outputs, :manifest_name
+
+    def initialize(hash = {})
+      hsh = ActiveSupport::HashWithIndifferentAccess.new(underscore_hash(hash))
+      super(hash)
+      @outputs = (hsh[:outputs] || []).map { |output| Bitmovin::Encoding::StreamOutput.new(output) }
+    end
+
+    def streams
+      if !persisted?
+        raise BitmovinError.new(self), "Needs to be persisted first"
+      end
+      HlsVariantStreamList.new(@id)
+    end
   end
 end
--- spec/bitmovin/encoding/manifests/hls_manifest_spec.rb
+++ spec/bitmovin/encoding/manifests/hls_manifest_spec.rb
@@ -5,6 +5,8 @@ describe Bitmovin::Encoding::Manifests::HlsManifest do
   it { should respond_to(:list).with(0..2).arguments }
 
   describe "instance" do
+    let(:manifest) { Bitmovin::Encoding::Manifests::HlsManifest.new }
+    let(:manifest_with_id) { Bitmovin::Encoding::Manifests::HlsManifest.new(id: 'manifest-id') }
     subject { Bitmovin::Encoding::Manifests::HlsManifest.new }
     it { should respond_to(:id) }
     it { should respond_to(:name) }
@@ -14,5 +16,55 @@ describe Bitmovin::Encoding::Manifests::HlsManifest do
     it { should respond_to(:outputs) }
     it { should respond_to(:manifest_name) }
     it { should be_a(Bitmovin::Resource) }
+
+    it { should respond_to(:streams) }
+    it { should respond_to(:media) }
+
+    let(:output_json) { 
+      {
+        outputId: "55354be6-0237-42bb-ae85-a2d4ef1ed19e",
+        outputPath: "/encodings/movies/movie-1/video_720/",
+        acl: [
+          {
+            permission: "PUBLIC_READ"
+          }
+        ]
+      }
+    }
+
+    describe "streams" do
+      context "with unpersisted instance" do
+        subject { Bitmovin::Encoding::Manifests::HlsManifest.new }
+        it "should raise BitmovinError" do
+          expect { subject.streams }.to raise_error(BitmovinError)
+        end
+      end
+      context "with saved instance" do
+        subject { Bitmovin::Encoding::Manifests::HlsManifest.new(id: 'manifest-id') }
+        it "should return ManifestStreamList" do
+          expect(subject.streams).to be_a(Bitmovin::Encoding::Manifests::HlsVariantStreamList)
+          expect(subject.streams.manifest_id).to eq('manifest-id')
+        end
+      end
+    end
+
+    describe "outputs" do
+      let(:manifest) { Bitmovin::Encoding::Manifests::HlsManifest.new({ id: 'encoding-id',
+                                                                         outputs: [output_json]}) }
+      subject { manifest.outputs }
+
+      it { should be_a(Array) }
+      it "should contain StreamOutput" do
+        expect(manifest).to have(1).outputs
+        expect(subject.first).to be_a(Bitmovin::Encoding::StreamOutput)
+      end
+
+      it "should initialize StreamOutput correctly" do
+        expect(subject.first.output_id).to eq("55354be6-0237-42bb-ae85-a2d4ef1ed19e")
+        expect(subject.first.output_path).to eq("/encodings/movies/movie-1/video_720/")
+        expect(subject.first.acl).to be_a(Array)
+        expect(subject.first.acl.first[:permission]).to eq("PUBLIC_READ")
+      end
+    end
   end
 end
