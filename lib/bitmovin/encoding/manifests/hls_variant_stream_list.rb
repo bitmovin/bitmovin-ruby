015152e4f2e07e7b5cea03b95aac732370043568
Daniel Hoelbling-Inzko <daniel@hoelbling-inzko.com>
2017-10-11 12:50:31 +0200

    Added variant stream list to hls manifests

---
+++ lib/bitmovin/encoding/manifests/hls_variant_stream_list.rb
@@ -0,0 +1,18 @@
+module Bitmovin::Encoding::Manifests
+  class HlsVariantStreamList < Bitmovin::Resource
+    init "streams"
+    attr_accessor :manifest_id
+
+    def initialize(manifest_id)
+      @manifest_id = manifest_id
+      self.class.init(File.join("/v1/encoding/manifests/", manifest_id, "streams"))
+      #super(hsh)
+    end
+
+    def list(limit = 100, offset = 0)
+      path = File.join("/v1/encoding/manifests/hls", @manifest_id, self.class.resource_path)
+      response = Bitmovin.client.get(path, { limit: limit, offset: offset })
+      result(response)['items'].map { |item| self.class.klass.new(@encoding_id, item) }
+    end
+  end
+end
